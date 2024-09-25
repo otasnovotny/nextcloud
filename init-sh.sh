1431fb79d9d97e891e69434a3779f841d2dbfe2c
Ota Novotný <otas.novotny@gmail.com>
2024-09-25 03:30:39 -0400

    Nextcloud / Otasovo.cz: .knownhost settings

--- init-sh.sh
+++ init-sh.sh
@@ -1,3 +1,18 @@
-git clone git@github.com:otasnovotny/nextcloud-override.git
+git clone git@github.com:otasnovotny/nextcloud.git
 
-# create db.env
+# -----------------
+# OCC console - get into nextcloud-app-1 container:
+docker exec -it --user www-data nextcloud-app-1 bash
+
+# then
+# Solving error about cron failed
+php occ config:system:set default_phone_region --value="CZ"
+php occ files:scan --all
+php -f ./cron.php
+
+# remove error (extra file)
+sudo rm ~/html/nextcloud-init-sync.lock
+
+# -----------------
+# OR from Host machine directly to container, like:
+docker exec --user www-data nextcloud-app-1 php occ config:system:set default_phone_region --value="CZ"
